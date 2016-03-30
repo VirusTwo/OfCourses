dc2fdceb82d10c0ac62fba80a4d958d6ecd87a31
martinezguillaume <martinezguillaume@users.noreply.github.com>
2016-03-30 21:42:08 +0200

    Add working connexion + error message + page redirection 😱

--- OfCourses/app/src/main/java/iutorsaytpc/ofcourses/fragment/LoadingFragment.java
+++ OfCourses/app/src/main/java/iutorsaytpc/ofcourses/fragment/LoadingFragment.java
@@ -1,5 +1,6 @@
 package iutorsaytpc.ofcourses.fragment;
 
+import android.annotation.SuppressLint;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,18 +8,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import iutorsaytpc.ofcourses.R;
+import iutorsaytpc.ofcourses.view.LoadingView;
 
 /**
  * Created by VirusTwoIUT on 11/03/2016.
  */
+@SuppressLint("ValidFragment")
 public class LoadingFragment extends Fragment {
     private View view;
 
-    public LoadingFragment(){}
+    public LoadingFragment(LoadingView view) {
+        this.view = view;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        this.view= inflater.inflate(R.layout.loading_fragment,container,false);
         return this.view;
     }
 
