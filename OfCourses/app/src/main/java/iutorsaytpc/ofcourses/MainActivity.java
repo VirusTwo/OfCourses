5f32b6e060d2e7ecef550ab8e5a74d09d7a1d87e
CREBOUW <cyril.crebouw@u-psud.fr>
2016-04-06 12:55:22 +0200

    Modif

--- OfCourses/app/src/main/java/iutorsaytpc/ofcourses/MainActivity.java
+++ OfCourses/app/src/main/java/iutorsaytpc/ofcourses/MainActivity.java
@@ -59,6 +59,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onPostResume() {
+        //Update de la toolbar
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         //Si la vue active est null alors on affiche la vue connexion, sinon on réaffiche la vue active
         if(currentView==null){
             ConnexionView connexionView = new ConnexionView(this);
